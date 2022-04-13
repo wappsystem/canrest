8a2c25890b7ef858c1278f8421bd83a0c9dbed1b
Gunnar Unger <gunnar.unger@gmail.com>
2022-04-13 16:13:30 +1000

    Update

--- module-list.js
+++ module-list.js
@@ -4,12 +4,15 @@
         "not_elig1":         {url:"$H/m/not_elig1.html"},
         "not_elig2":         {url:"$H/m/not_elig2.html"},
         "not_elig3":         {url:"$H/m/not_elig3.html"},
+        "not_elig4":         {url:"$H/m/not_elig4.html"},
         "recruitment-data": {url:"$H/m/elig-questions-data.html",Table:"canrest-recruitment-record",form_module:"recruitment-form",router:1},
         "recruitment-form": {url:"$H/m/elig-questions-form.html",Table:"canrest-recruitment-record"},
         "recruitment-1-data": {url:"$H/m/elig-1-questions-data.html",Table:"canrest-recruitment-record"},
         "recruitment-2-data": {url:"$H/m/elig-2-questions-data.html",Table:"canrest-recruitment-record"},
         "recruitment-3-data": {url:"$H/m/elig-3-questions-data.html",Table:"canrest-recruitment-record"},
-        "recruitment-elig-data": {url:"$H/m/elig-all-questions-data.html",Table:"canrest-recruitment-record"},
+        "recruitment-4-data": {url:"$H/m/elig-4-questions-data.html",Table:"canrest-recruitment-record"},
+        "recruitment-elig-data": {url:"$H/m/elig-all-questions-data.html",Table:"canrest-recruitment-record",form_module:"recruitment-elig-form",task_name:"Eligible"},
+        "recruitment-elig-form": {url:"$H/m/elig-all-questions-form.html",Table:"canrest-recruitment-record",task_name:"Eligible"},
     }
     for(p in modules){
         $vm.module_list[p]=modules[p];
